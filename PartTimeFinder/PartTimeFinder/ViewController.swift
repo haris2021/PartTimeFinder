f63cf33056ceecdc81bce2a206d561e3d7887da7
Rahul Chandak <chandak.r@northeastern.edu>
2024-11-19 15:46:19 -0500

    Job Details Screen

--- PartTimeFinder/PartTimeFinder/ViewController.swift
+++ PartTimeFinder/PartTimeFinder/ViewController.swift
@@ -12,6 +12,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Home Screen"
+        
+        let jobDescription = JobDescriptionViewController()
+        self.navigationController?.pushViewController(jobDescription, animated: true)
     }
 
 

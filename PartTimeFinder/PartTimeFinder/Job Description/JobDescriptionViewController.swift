bcc550e4bc193b83e887d380561be9c85619c2c4
Rahul Chandak <chandak.r@northeastern.edu>
2024-11-19 17:04:48 -0500

    Job Details

---
+++ PartTimeFinder/PartTimeFinder/Job Description/JobDescriptionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  JobDescriptionViewController.swift
+//  PartTimeFinder
+//
+//  Created by Rahul Chandak on 11/19/24.
+//
+
+import UIKit
+
+class JobDescriptionViewController: UIViewController {
+
+    let jobDescription = JobDescription()
+    
+    override func loadView() {
+        view = jobDescription
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Job Description"
+        
+        // Do any additional setup after loading the view.
+    }
+
+}
